d9bfb086c4cef05a25a57b72e688a8b5ed04e761
codebrahma <dkbishwash@gmail.com>
2019-06-19 18:53:25 +0530

    feat: added Home module
    added Home module for showing the public gallery albums.

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,10 @@
-import React, { lazy, Suspense } from "react";
-import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import React, { lazy, Suspense } from 'react';
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 const render = Comp => props => <Comp {...props} />;
-const Home = render(lazy(() => import("./modules/Home")));
+const Home = render(lazy(() => import('./modules/Home')));
 
 const Routes = () => (
   <Suspense fallback={<div>loading...</div>}>
