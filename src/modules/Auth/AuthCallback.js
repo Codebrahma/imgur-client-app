9110967aa1130e0826acfbaa5601203fbb51537a
DivyanshBatham <divyansh.sunita@gmail.com>
2019-06-25 12:58:28 +0530

    Hash Validation and Redirects

--- src/modules/Auth/AuthCallback.js
+++ src/modules/Auth/AuthCallback.js
@@ -5,19 +5,53 @@ import { location } from '../../routerPropTypes';
 
 class AuthCallback extends Component {
   static contextType = AuthContext;
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isInvalidHash: !this.isValidHash(this.props.location.hash),
+    };
+  }
+
+
+  componentDidMount() {
+    const {
+      access_token: accessToken,
+      extractParamsAndSaveToLocalStorageAndState,
+    } = this.context;
+    if (!accessToken) {
+      extractParamsAndSaveToLocalStorageAndState(this.props.location.hash);
+    }
+  }
+
+  isValidHash = hash => /^#access_token=[a-z\d]+&expires_in=\d+&token_type=bearer&refresh_token=[a-z\d]+&account_username=\w+&account_id=\d+$/.test(hash);
+
   render() {
-    const { access_token: accessToken, extractParamsAndSaveToLocalStorageAndState } = this.context;
+    if (this.state.isInvalidHash) return <Redirect to="/" />;
 
-    if (accessToken) { return <Redirect to="/dashboard" />; }
+    const {
+      access_token: accessToken,
+      account_username: username,
+    } = this.context;
 
-    // TODO: Validate the hash, and How to handle invalid hash?
-    extractParamsAndSaveToLocalStorageAndState(this.props.location.hash);
-    return <Redirect to="/dashboard" />;
+    // REDIRECT TO A FIXED ROUTE:
+    // TODO: Get it working for Private Routes.
+    // If redirected from Private Route with location.state props, redirect there after login.
+    // Or redirect to the default path.
+    const { from } = this.props.location.state || { from: { pathname: `/user/${username}` } };
+    if (accessToken) return <Redirect to={from} />;
+
+    // REDIRECT TO WHATEVER ROUTE THE USER PREVIOUSLY WAS:
+    // BUG: This is reloading the entire app.
+    // if (accessToken) this.props.history.goBack();
+
+    return 'Loading';
   }
 }
 
 AuthCallback.propTypes = {
   location: location.isRequired,
+  // history: history.isRequired,
 };
 
 export default AuthCallback;
