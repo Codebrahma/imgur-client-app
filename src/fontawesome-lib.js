29e0f2f1189707849f6c918ca508d03ce66ddb2c
DivyanshBatham <divyansh.sunita@gmail.com>
2019-07-01 13:44:25 +0530

    FontAwesome Added, Library Created

---
+++ src/fontawesome-lib.js
@@ -0,0 +1,13 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+import {
+  faEye, faEdit, faComment,
+  faArrowAltCircleUp, faArrowAltCircleDown,
+  faReply, faPlus, faChevronDown, faEllipsisH,
+} from '@fortawesome/free-solid-svg-icons';
+
+library.add(
+  faEye, faEdit, faComment,
+  faArrowAltCircleUp, faArrowAltCircleDown,
+  faReply, faPlus, faChevronDown, faEllipsisH,
+);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.scss";
-import Routes from "./routes";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import Routes from './routes';
+import './fontawesome-lib';
 
 const Index = () => <Routes />;
 
-ReactDOM.render(<Index />, document.getElementById("index"));
+ReactDOM.render(<Index />, document.getElementById('index'));
