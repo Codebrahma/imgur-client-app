f10e7b011ebf2aadbcee7056c3658d2b6c1f1b40
DivyanshBatham <divyansh.sunita@gmail.com>
2019-07-10 18:16:31 +0530

    Converted CardRenderer into a dummy component.
    - State is lifted up the parent components (Home and Favorites)
    
    TODO: Make it robust:
    [] Clear incomplete requests
    [] Handle state changes properly

--- src/modules/Home/index.js
+++ src/modules/Home/index.js
@@ -1,15 +1,115 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Grid } from 'react-flexbox-grid';
+import axios from 'axios';
 import CardRenderer from '../CardRenderer';
 
-const Home = () => (<CardRenderer
-// https://api.imgur.com/3/gallery/{{section}}/{{sort}}/{{window}}/{{page}}
-//            ?showViral={{showViral}}&mature={{showMature}}&album_previews={{albumPreviews}}
-  generateUrl={currentPage => `https://api.imgur.com/3/gallery/hot/viral/all/${currentPage}/`}
-  params={{
-    showViral: true,
-    mature: true,
-    album_previews: true,
-  }}
-/>);
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {
+        viral: [],
+        top: [],
+        time: [],
+        rising: [],
+      },
+      currentPage: {
+        viral: 0,
+        top: 0,
+        time: 0,
+        rising: 0,
+      },
+      section: 'hot',
+      sort: 'viral', // Default
+      loading: false,
+      loadMoreData: true,
+    };
+  }
+
+  changeSort = (sort) => {
+    this.setState(
+      { sort },
+      () => {
+        this.fetchData();
+      },
+    );
+  }
+
+  fetchData = () => {
+    const { currentPage, sort, section } = this.state;
+    this.setState({ loading: true }, () => {
+      axios({
+        method: 'get',
+        url: `https://api.imgur.com/3/gallery/${section}/${sort}/all/${currentPage[sort]}`,
+        // params: this.props.params,
+        // TODO: Add some kind of controller.
+        // params={{
+        //   showViral: true,
+        //   mature: true,
+        //   album_previews: true,
+        // }}
+        headers: {
+          Authorization: `Client-ID ${process.env.CLIENT_ID}`,
+        },
+      })
+        .then((res) => {
+          this.setState(prevState => ({
+            data: {
+              ...prevState.data,
+              [sort]: [...prevState.data[sort], ...res.data.data.map(dataItem => ({
+                id: dataItem.id,
+                images: dataItem.images,
+                title: dataItem.title,
+                ups: dataItem.ups,
+                downs: dataItem.downs,
+                views: dataItem.views,
+                commentCount: dataItem.comment_count,
+                link: dataItem.link,
+              }))],
+            },
+            currentPage: {
+              ...prevState.currentPage,
+              [sort]: prevState.currentPage[sort] + 1,
+            },
+            loading: false,
+            loadMoreData: (res.data.data.length > 0), // data was fetched try again to fetch more data.
+          }));
+        })
+        .catch(err => console.log(err));
+    });
+  };
+
+  controls = () => (
+    <div className="cardRenderer__controls--favorites">
+      <h1>Home</h1>
+
+      <div>
+        <button onClick={() => this.changeSort('viral')}>Popular</button>
+        <button onClick={() => this.changeSort('top')}>Top</button>
+        <button onClick={() => this.changeSort('time')}>Newest</button>
+      </div>
+    </div>
+  )
+
+
+  render() {
+    const {
+      data, loading, sort, loadMoreData,
+    } = this.state;
+
+    return (
+      <Grid>
+        <h1>Home</h1>
+        <CardRenderer
+          controls={this.controls}
+          loading={loading}
+          data={data[sort]}
+          fetchData={this.fetchData}
+          loadMoreData={loadMoreData}
+        />
+      </Grid>
+    );
+  }
+}
 
 export default Home;
--- src/modules/User/Favorites.js
+++ src/modules/User/Favorites.js
@@ -1,21 +1,100 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import { Grid } from 'react-flexbox-grid';
+import PropTypes from 'prop-types';
+import axios from 'axios';
 import CardRenderer from '../CardRenderer';
 
 class Favorites extends Component {
   constructor(props) {
     super(props);
-    this.state = { };
+    this.state = {
+      data: {
+        newest: [],
+        oldest: [],
+      },
+      currentPage: {
+        newest: 0,
+        oldest: 0,
+      },
+      favoritesSort: 'newest',
+      loading: false,
+      loadMoreData: true,
+    };
   }
-  render() {
+
+  changeSort = (sort) => {
+    this.setState(
+      { favoritesSort: sort },
+      () => {
+        this.fetchData();
+      },
+    );
+  }
+
+  fetchData = () => {
     const { username } = this.props.match.params;
+    const { currentPage, favoritesSort } = this.state;
+
+    this.setState({ loading: true }, () => {
+      axios({
+        method: 'get',
+        url: `https://api.imgur.com/3/account/${username}/gallery_favorites/${currentPage[favoritesSort]}/${favoritesSort}`,
+        headers: {
+          Authorization: `Client-ID ${process.env.CLIENT_ID}`,
+        },
+      })
+        .then((res) => {
+          this.setState(prevState => ({
+            data: {
+              ...prevState.data,
+              [favoritesSort]: [...prevState.data[favoritesSort], ...res.data.data.map(dataItem => ({
+                id: dataItem.id,
+                images: dataItem.images,
+                title: dataItem.title,
+                ups: dataItem.ups,
+                downs: dataItem.downs,
+                views: dataItem.views,
+                commentCount: dataItem.comment_count,
+                link: dataItem.link,
+              }))],
+            },
+            currentPage: {
+              ...prevState.currentPage,
+              [favoritesSort]: prevState.currentPage[favoritesSort] + 1,
+            },
+            loading: false,
+            loadMoreData: (res.data.data.length > 0), // data was fetched try again to fetch more data.
+          }));
+        })
+        .catch(err => console.log(err));
+    });
+  };
+
+  controls = () => (
+    <div className="cardRenderer__controls--favorites">
+      <h1>All Favorites</h1>
+
+      <div>
+        <button onClick={() => this.changeSort('newest')}>Newest</button>
+        <button onClick={() => this.changeSort('oldest')}>Oldest</button>
+      </div>
+    </div>
+  )
+
+  render() {
+    const {
+      data, loading, favoritesSort, loadMoreData,
+    } = this.state;
+
     return (
       <Grid>
         <h1>Favorites</h1>
         <CardRenderer
-        // https://api.imgur.com/3/account/{{username}}/favorites/{{page}}/{{favoritesSort}}
-          generateUrl={currentPage => `https://api.imgur.com/3/account/${username}/gallery_favorites/${currentPage}/new`}
+          controls={this.controls}
+          loading={loading}
+          data={data[favoritesSort]}
+          fetchData={this.fetchData}
+          loadMoreData={loadMoreData}
         />
       </Grid>
     );
